0eb73b1d0350148ffa0e7ed867a7a4fa0056b291
Daniel Stollnberger <daniiistolliii@gmail.com>
2025-11-18 05:55:09 +0100

    world created

--- js/game.js
+++ js/game.js
@@ -1,8 +1,10 @@
 let canvas;
 let ctx;
-let character = new Character(400, 200);
+let world = new World(); 
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
+
+    console.log('My character is', world.character);
 }
